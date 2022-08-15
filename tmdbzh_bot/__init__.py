234577bcb62f7401b7565bc779583a895cfcfcb7
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2022-08-15 11:08:28 -0400

    properly handle config file

--- tmdbzh_bot/__init__.py
+++ tmdbzh_bot/__init__.py
@@ -1,6 +1,11 @@
 import tomli
+import os
 
 from pyrogram import Client
+
+if not os.path.exists("config.toml"):
+    print("请复制config-sample.toml为config.toml并修改")
+    exit()
 
 with open("config.toml", "rb") as f:
     CONFIG = tomli.load(f)
